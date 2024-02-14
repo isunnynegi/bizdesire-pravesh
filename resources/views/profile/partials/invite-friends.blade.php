24791c8b5b6279880ba0e155efff3816f7101cad
Pravesh Negi <npravesh@teqmavens.com>
2024-02-14 18:35:23 +0530

    test

---
+++ resources/views/profile/partials/invite-friends.blade.php
@@ -0,0 +1,11 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Invite Friends') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __("Refer your friends to get cashback.") }}
+        </p>
+    </header>
+</section>
