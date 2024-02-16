ac1b5d0c86574a11d8bf4cc127af730a0ccca090
Pravesh Negi <npravesh@teqmavens.com>
2024-02-16 12:06:44 +0530

    update add to cart function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,8 +10,6 @@ use DB;
 class CartController extends Controller
 {
     function addtocart(Request $request, string $id ){
-
-        $userId = Auth::user()->id;
         try {
             if(isset($id) && !empty($id)){
                 /**
@@ -21,44 +19,28 @@ class CartController extends Controller
 
                 DB::beginTransaction();
                 if($product){
-                    $cart = Cart::firstOrCreate([
-                        'user_id' => $userId
-                    ],[
-                        'subtotal' => 0,
-                        'total' => 0
-                    ]);
+                    /** 
+                     * Create Or Get Cart Instance for loggedin User
+                     */
+                    $cart = $this->getUserCart();
 
+                    /**
+                     * Add Or Update Items to the cart
+                     */
+                    $this->addUpdateCartItems($cart->id, $product);
 
-                    $cartDetail = CartDetail::firstOrNew([
-                        'cart_id' => $cart->id,
-                        'product_id' => $product->id
-                    ]);
+                    /**
+                     * Get Cart items total
+                     */
+                    $productstotal = $this->getCartProductTotal($cart->id);
 
-                    if($cartDetail->exists()){
-                        $cartDetail->qty = $cartDetail->qty + 1;
-                    } else {
-                        $cartDetail->qty = 1;
-                    }
-                    $cartDetail->price = $product->price;
-                    $cartDetail->save();
+                    /**
+                     * update cart pricing
+                     */
+                    $this->updateCart($cart->id, $productstotal);
 
-                    $cartDetail = CartDetail::where('cart_id',$cart->id)->get();
-
-                    if(isset($cartDetail) && !empty($cartDetail)){
-                        $productstotal = $cartDetail->reduce(function ($total,$value,$key) {
-                            return $total + ($value['qty'] * $value['price']);
-                        });
-
-                        $cart = Cart::findOrFail($cart->id);
-                        $cart->subtotal = $productstotal;
-                        $cart->total = $productstotal;
-                        $cart->save();
-
-                        DB::commit();
-                        return redirect()->back()->with('success', 'Product Added.');
-                    } else {
-                        throw new Exception("No item not found in cart.", 422);    
-                    }
+                    DB::commit();
+                    return redirect()->back()->with('success', 'Product Added.');
                     
                 } else {
                     throw new Exception("Product not found.", 422);    
@@ -67,11 +49,59 @@ class CartController extends Controller
                 throw new Exception("Not a valid request.", 422);    
             }
         } catch (\Exception $e) {
-            //throw $th;
             DB::rollBack();
             $message = $e->getMessage();
-            dd($message);
             return redirect()->back()->with('error',$message);
         }
+    }
+
+    private function getUserCart(){
+        $userId = Auth::user()->id;
+
+        $cart = Cart::firstOrCreate([
+            'user_id' => $userId,
+        ], [
+            'subtotal' => 0,
+            'total' => 0,
+        ]);
+
+        return $cart;
+
+    }
+
+    private function addUpdateCartItems($cartId, $product){
+        $cartDetail = CartDetail::firstOrNew([
+            'cart_id' => $cartId,
+            'product_id' => $product->id
+        ]);
+
+        if($cartDetail->exists()){
+            $cartDetail->qty = $cartDetail->qty + 1;
+        } else {
+            $cartDetail->qty = 1;
+        }
+        $cartDetail->price = $product->price;
+        $cartDetail->save();
+    }
+
+    private function getCartProductTotal($cartId){
+        $cartDetail = CartDetail::where('cart_id', $cartId)->get();
+
+        if (isset($cartDetail) && !empty($cartDetail)) {
+            $productstotal = $cartDetail->reduce(function ($total, $value, $key) {
+                return $total + ($value['qty'] * $value['price']);
+            });
+
+           return $productstotal;
+        } else {
+            throw new Exception("No item not found in cart.", 422);
+        }
+    }
+
+    private function updateCart($cartId, $productstotal){
+        $cart = Cart::findOrFail($cartId);
+        $cart->subtotal = $productstotal;
+        $cart->total = $productstotal;
+        $cart->save();
     }
 }
